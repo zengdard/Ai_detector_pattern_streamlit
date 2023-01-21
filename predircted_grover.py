cdc91fa2e6598b9fdd5a0bccd360fae0558978a0
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-01-21 18:27:10 +0100

    Update predircted_grover.py

--- predircted_grover.py
+++ predircted_grover.py
@@ -7,13 +7,13 @@ st.title('StendhalGPT')
 token = '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'
 API_URL = "https://api-inference.huggingface.co/models/roberta-large-openai-detector"
 headers = {"Authorization": f"Bearer {token}"}
-st.text('Cet outil est en version de développement.' )
+st.text('Cet outils est en version de développement.' )
 def query(payload):
     
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-default_value_goes_here = 'Quel est votre texte ? (Anglais Uniquement).'
+default_value_goes_here = 'Quel est votre texte ? (Anglais Uniquement et de taille moyenne).'
 user_input = st.text_input("Votre texte ici.", default_value_goes_here)
 
 
@@ -24,9 +24,9 @@ if st.button('Vérifier'):
         })
     try:
         if output[0][1]['score'] >=  output[0][0]['score']:
-            st.text('Votre texte est à'+ str(round(output[0][0]['score'], 2)*100)+'% de chance d\'être écrit par un homme.')    
+            st.text('Votre texte est à'+ str(round(output[0][0]['score'], 2)*100)+'% de chance d\'être génèré par une IA.')    
         else : 
-            st.text('Votre texte est à '+ str(round(output[0][1]['score'], 2)*100)+'% de chance d\'être génèré par une IA.' ) 
+            st.text('Votre texte est à '+ str(round(output[0][1]['score'], 2)*100)+'% de chance d\'être écrit par un humain.' ) 
     except:
         st.text('Le service est temporairement indisponible, merci de votre compréhension.' )
 
