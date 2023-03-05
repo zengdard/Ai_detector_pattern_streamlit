754ce96469db8b6e8a00e94241561a29c9279772
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-03-05 15:23:20 +0100

    Update predircted_grover.py

--- predircted_grover.py
+++ predircted_grover.py
@@ -302,7 +302,7 @@ if tabs =='Quésaco ?':
 
     st.subheader('La différence relative')
 
-    st.markdown('La différence relative permet de mettre en évidence les différences significatives entre deux textes. Plus cet indicateur est élevé plus celui-ci indique que les textes sont significativement différents les des autres. ')
+    st.markdown('La différence relative permet de mettre en évidence les différences significatives entre deux textes. Plus cet indicateur est élevé plus celui-ci indique que les textes sont significativement différents les uns des autres. ')
 
 
    ## st.subheader("Comment fonctionne la comparaison de deux modèles de Markov ?")
